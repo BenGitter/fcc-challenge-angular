f4e24edf3a6563b7f2c7a993e9f483ca4efe0164
Benjamin <benjaminvanzwienen@live.nl>
2016-11-18 21:55:54 +0100

    add divs for icons

--- Front-End/src/app/app.module.ts
+++ Front-End/src/app/app.module.ts
@@ -6,12 +6,14 @@ import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { HeroComponent } from './hero/hero.component';
+import { InfoComponent } from './info/info.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    HeroComponent
+    HeroComponent,
+    InfoComponent
   ],
   imports: [
     BrowserModule,
