e2320bec241c10a29d52968d309a8b6188b49fd4
Benjamin <benjaminvanzwienen@live.nl>
2016-11-20 23:15:06 +0100

    Complete landing page

--- Front-End/src/app/app.module.ts
+++ Front-End/src/app/app.module.ts
@@ -2,12 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { HeroComponent } from './hero/hero.component';
 import { InfoComponent } from './info/info.component';
 import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { TimerComponent } from './timer/timer.component';
+
+const appRoutes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'timer', component: TimerComponent },
+  { path: '**', component: HomeComponent }
+];
+
 
 @NgModule({
   declarations: [
@@ -15,12 +25,15 @@ import { FooterComponent } from './footer/footer.component';
     NavbarComponent,
     HeroComponent,
     InfoComponent,
-    FooterComponent
+    FooterComponent,
+    HomeComponent,
+    TimerComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
